06fab84954047d74c2ec079f34e9b43b2805e5fe
Alan-0524 <119241066@163.com>
2018-10-03 23:49:43 +1300

    update some code

--- index.js
+++ index.js
@@ -129,13 +129,13 @@ function handlePostback(sender_psid, received_postback) {
   let payload = received_postback.payload;
 
   // Set the response based on the postback payload
-  if (payload === 'yes') {
+  if (payload === '0') {
     response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
+  } else if (payload === '1') {
     response = { "text": "Oops, try sending another image." }
   }
   // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
+  //callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API

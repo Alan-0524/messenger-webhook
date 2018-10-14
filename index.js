632ea1e579b93afef9a3862ac4d2735a262fd3fc
Alan-0524 <119241066@163.com>
2018-10-14 19:50:05 +1300

    update

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function handleMessage(sender_psid, received_message) {
     "method": "GET"
 	}, (err, body) => {
     if (!err) {
-	  console.log("status--------------------"+body.get('status'));
+	  console.log("status--------------------"+body[0]);
     } else {
       console.error("Unable to send message:" + err);
     }
